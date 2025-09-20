c329b175b07d25cb4f4b1f7519d1368f7ea3b407
Storm-Verlegh <36988@lln.hethwc.nl>
2025-09-20 11:42:57 +0200

    Update 2_6_Variabelen_1-5.py

--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,14 @@
 # opdracht 1 tpt en met 5 van paragraaf 2.6
+postcode = "3343 PL"
+print(postcode.lower())
+woonplaats = "Hendrik-Ido-Ambacht"
+print(woonplaats.upper())
+verzendmethode = "PostNL"
+betaalmethode = "iDeal"
+print(verzendmethode.lower())
+print(betaalmethode.upper())
+postcode  = input("Wat is je postcode?")
+print(postcode.upper())
+plaats = "Gasselternijveenschemond"
+print(len(plaats))
+''git commit''
