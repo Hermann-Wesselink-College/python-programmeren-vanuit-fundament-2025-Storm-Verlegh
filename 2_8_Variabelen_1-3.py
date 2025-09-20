96ded87d6ba9d1a0aa9680eb53d63f46596c956c
Storm-Verlegh <36988@lln.hethwc.nl>
2025-09-20 11:47:07 +0200

    Update 2_8_Variabelen_1-3.py

--- 2_8_Variabelen_1-3.py
+++ 2_8_Variabelen_1-3.py
@@ -1,1 +1,18 @@
 # opdrachten 1 tot en met 3 van pragraaf 2.8
+prijs = 24.49
+print("Prijs: " + str(prijs))
+voornaam = "Henk"
+achternaam = "de Vries"
+naam = voornaam + " " + achternaam
+naamLengte = len(naam)
+print("Mijn naam is " + naam)
+print("Mijn naam bestaat uit " + str(naamLengte) + " letters")
+print(voornaam.lower())
+print(achternaam.upper())
+voornaam = input("Wat is je voornaam? ")
+achternaam = input("Wat is je achternaam? ")
+naam = voornaam + " " + achternaam
+naamLengte = len(naam)
+print("Je naam is: " + naam)
+print("Je naam bestaat uit " + str(naamLengte) + " letters (inclusief spaties)")
+''git commit''
