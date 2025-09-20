e31cf7177a0bd6b28e82ec29772741f008af206c
Storm-Verlegh <36988@lln.hethwc.nl>
2025-09-20 11:38:00 +0200

    Update 2_4_Variabelen_1-2.py

--- 2_4_Variabelen_1-2.py
+++ 2_4_Variabelen_1-2.py
@@ -1,1 +1,8 @@
 # Opgave 1 en 2 van paragraaf 2.4 
+voornaam=input(''Wat is je voornaam?'')
+print(voornaam)
+voornaam=input(''Wat is je voornaam?'')
+print(''Je voornaam is:'')
+print(voornaam)
+''git commit''
+      
