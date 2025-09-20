3aea6ad3103cbe737daa83b5b4822afbed892a20
Storm-Verlegh <36988@lln.hethwc.nl>
2025-09-20 11:50:21 +0200

    Update 2_9_Variabelen_1-6.py

--- 2_9_Variabelen_1-6.py
+++ 2_9_Variabelen_1-6.py
@@ -1,1 +1,27 @@
 # opdrachten 1 tot en met 6 van paragraaf 2.9
+totaalprijs = 100
+kortingsBon = 25
+nieuweprijs = totaalprijs - kortingsBon
+print(nieuweprijs)
+	cijfer1 = 7.2
+cijfer2 = 8.6
+gemiddelde = (cijfer1 + cijfer2) / 2
+print(gemiddelde)
+prijsExclBtw = float(input("Voer een prijs in:"))
+prijsInclBtw = prijsExclBtw * 1.21
+print(prijsInclBtw)
+	totaalprijs = float(input("Totaalprijs van de producten:"))
+verzendkosten = 3.95
+teBetalen = totaalprijs + verzendkosten
+print("Totaalprijs: " + str(totaalprijs) + " EUR.")
+print("Verzendkosten: " + str(verzendkosten) + " EUR.")
+print("TOTAAL: " + str(teBetalen) + " EUR.")
+bedrag = float(input("Voer een bedrag in euros in"))
+wisselkoers = 1.1648
+prijsDollar = bedrag * wisselkoers
+totaalPrijs = float(input("Geef een totaalprijs:"))
+kortingsPercentage = float(input("Geef een kortingspercentage:"))
+nieuwePrijs = (1 - (kortingsPercentage / 100)) * totaalPrijs
+print(nieuwePrijs)
+''git commit''
+print("$" + str(prijsDollar))
