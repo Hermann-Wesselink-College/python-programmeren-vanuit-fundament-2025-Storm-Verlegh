0f2aa6587510780d6a37d73ab466d37d90c563bb
Storm-Verlegh <36988@lln.hethwc.nl>
2025-09-20 11:45:59 +0200

    Update 2_7_Variabelen_1-2.py

--- 2_7_Variabelen_1-2.py
+++ 2_7_Variabelen_1-2.py
@@ -1,1 +1,13 @@
 # opdracht 1 en 2 van paragraaf 2.7
+voornaam = input("Wat is je voornaam?")
+achternaam = input("Wat is je achternaam?")
+print("Hallo " + voornaam + " " + achternaam + "!")
+naam = input("Wat is je naam?")
+straat = input("In welke straat woon je?")
+huisnummer = input("Wat is je huisnummer?")
+postcode = input("Wat is je postcode?")
+woonplaats = input("Wat is je woonplaats?")
+print(naam)
+print(straat + " " + huisnummer)
+print(postcode.upper() + "  " + woonplaats.upper())
+''git commit''
