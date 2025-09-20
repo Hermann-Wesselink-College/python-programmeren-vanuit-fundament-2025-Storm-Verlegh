39038106d1ad3e08bfade0923afb39110aeefb08
Storm-Verlegh <36988@lln.hethwc.nl>
2025-09-20 11:34:58 +0200

    Update 2_1_Variabelen_1-4.py

--- 2_1_Variabelen_1-4.py
+++ 2_1_Variabelen_1-4.py
@@ -1,1 +1,10 @@
 # opgaven 1 tot en met 4 van paragraaf 2.1 Variabelen
+verzendkosten=5
+productcode=''A136i
+verzendkosten=5
+print(verzendkosten)
+productcode=''A136i
+print(productcode)
+prijs=9,95
+print(prijs)
+''git commit''
